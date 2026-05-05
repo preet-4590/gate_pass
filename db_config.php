890a241248f8d10cc9fa21579dddec4ce43277ae
Manpreet <preetm4590@gmail.com>
2026-05-05 10:41:06 +0530

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -1,5 +1,7 @@
 <?php
 $conn = mysqli_connect("localhost", "root", "", "gate_pass");
+$site_url = "https://glandular-barcode-kitten.ngrok-free.dev";
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
